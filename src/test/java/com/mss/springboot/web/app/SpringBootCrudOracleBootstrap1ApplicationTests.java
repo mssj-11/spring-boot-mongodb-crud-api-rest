9c654de11ec2e334bda81ba5bf82ecc3215337b5
Mike <80673026+mssj-11@users.noreply.github.com>
2023-04-11 17:03:04 -0600

    Spring-Boot - MongoDB - CRUD API REST

---
+++ src/test/java/com/mss/springboot/web/app/SpringBootCrudOracleBootstrap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mss.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudOracleBootstrap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
