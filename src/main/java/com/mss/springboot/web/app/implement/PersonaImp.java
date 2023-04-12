e5c44df78f2ca464393a8393fba487b4240a3789
Mike <80673026+mssj-11@users.noreply.github.com>
2023-04-11 19:16:47 -0600

    Persona - Implementar Servicio

--- src/main/java/com/mss/springboot/web/app/implement/PersonaImp.java
+++ src/main/java/com/mss/springboot/web/app/implement/PersonaImp.java
@@ -1,20 +1,25 @@
 package com.mss.springboot.web.app.implement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
 import com.mss.springboot.web.app.commons.GenericServiceImp;
+import com.mss.springboot.web.app.dao.PersonaRepositorio;
 import com.mss.springboot.web.app.model.Persona;
 import com.mss.springboot.web.app.service.PersonaServicio;
 
 
+@Service
 public class PersonaImp extends GenericServiceImp<Persona, Long> implements PersonaServicio{
 
+	@Autowired
+	private PersonaRepositorio personaRepositorio;
+	
 	@Override
 	public CrudRepository<Persona, Long> getDao() {
 		// TODO Auto-generated method stub
-		return null;
+		return personaRepositorio;
 	}
-	
-	
 	
 }
