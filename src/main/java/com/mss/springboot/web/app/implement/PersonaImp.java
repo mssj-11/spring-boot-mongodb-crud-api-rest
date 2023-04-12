3cd72eae0f020252829fb7ba3a819176697dc17c
Mike <80673026+mssj-11@users.noreply.github.com>
2023-04-11 18:42:03 -0600

    Persona Implemento

--- src/main/java/com/mss/springboot/web/app/implement/PersonaImp.java
+++ src/main/java/com/mss/springboot/web/app/implement/PersonaImp.java
@@ -1,5 +1,10 @@
 package com.mss.springboot.web.app.implement;
 
-public class PersonaImp {
+import com.mss.springboot.web.app.model.Persona;
+import com.mss.springboot.web.app.service.PersonaServicio;
+
+public class PersonaImp extends GenericServiceImp<Persona, Long> implements PersonaServicio{
+	
+	
 	
 }
