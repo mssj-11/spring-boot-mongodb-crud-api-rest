8eeec684c9017b0d88111130ba0fbf69d78fca34
Mike <80673026+mssj-11@users.noreply.github.com>
2023-04-11 18:35:03 -0600

    Persona Servicio

--- src/main/java/com/mss/springboot/web/app/service/PersonaServicio.java
+++ src/main/java/com/mss/springboot/web/app/service/PersonaServicio.java
@@ -1,5 +1,11 @@
 package com.mss.springboot.web.app.service;
 
-public interface PersonaServicio {
+import com.mss.springboot.web.app.commons.GenericServiceAPI;
+import com.mss.springboot.web.app.model.Persona;
+
+
+public interface PersonaServicio extends GenericServiceAPI<Persona, Long>{
+	
+	
 	
 }
